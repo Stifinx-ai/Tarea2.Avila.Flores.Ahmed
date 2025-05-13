aa0d765ed6f8c98e4ccf461a13c79e75b4c8b327
Stifinx-ai <yarethavf@gmail.com>
2025-05-12 20:43:36 -0600

    Update Ejercicio3.c

--- Ejercicio3.c
+++ Ejercicio3.c
@@ -1,1 +1,18 @@
+//En una casa de cambio necesitan construir un programa que dado como dato una cantidad expresada en dólares, convierta esa cantidad en pesos.
 
+#include <stdio.h>
+
+int main()
+{
+    float Pesos=11.96;
+    float Dollar;
+    
+    printf("Inscriba la cantidad de Dollar:\n");
+    scanf("%f", &Dollar);
+    
+    Dollar=Dollar*Pesos;
+    
+    printf("La cantidad en pesos es: %.2f\n", Dollar);
+    
+    return 0;
+}
